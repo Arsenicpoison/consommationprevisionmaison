323c45cf9ab506e558cf4921230523c6afc199b0
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-07 22:18:22 +0200

    add previsions model

---
+++ app/models/previsions.py
@@ -0,0 +1,13 @@
+from django.db import models
+from app.models import Product
+
+
+class Prevision(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantite = models.CharField(max_length=100)
+    date= models.DateField()    
+    unit_price = models.CharField(max_length=100)
+    total_price = models.IntegerField(null='True')
+
+    def __str__(self) -> str:
+        return self.quantite + " " +str(self.product)
