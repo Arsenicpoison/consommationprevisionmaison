c120bbe7c17e154fa5dad1abbc93dc963b79fd77
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-08 00:08:17 +0200

    add filters in home view

--- app/views/home.py
+++ app/views/home.py
@@ -1,8 +1,42 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-
+from app.models import Prevision,Acheter
 def index(request):
+    total_prevision=Prevision.objects.count()
+    total_acheter=Acheter.objects.count()
+    if request.method == 'POST':
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        if start_date == "" or end_date == "":
+            return render(
+                request,
+                'app/home/index.html',
+            )
+        else:
+            previsions = Prevision.objects.filter(date__range=[start_date,end_date])
+            acheters = Acheter.objects.filter(date__range=[start_date,end_date])
+            total = 0
+            totale = 0
+            for prevision in previsions:
+                total += prevision.total_price
+            
+            for acheter in acheters:
+                totale +=acheter.total_price
+            return render(
+                request,
+                'app/home/index.html',
+                {
+                    'previsions': previsions,
+                    'total': total,
+                    'acheters': acheters,
+                    'totale': totale,
+                    'total_prevision': total_prevision,
+                    'total_acheter':total_acheter,
+                    
+                }
+            )
     return render(
         request,
-        'app/home/index.html'
+        'app/home/index.html',
     )
+    
