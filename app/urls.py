edae4014fbaa776b66cb205790e7be6f09fd35bc
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-07 22:37:12 +0200

    add category urls in app urls

--- app/urls.py
+++ app/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from app.views import home
+from app.views import home,categories
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',home.index,name='home'),
+
+    path('categories/', categories.index, name='categories_index'),
+    path('categories/add', categories.add, name='categories_add'),
+    path('categories/store', categories.store, name='categories_store'),
+    path('categories/edit/<int:id>', categories.edit, name='categories_edit'),
+    path('categories/delete/<int:id>', categories.delete, name='categories_delete'),
 ]
