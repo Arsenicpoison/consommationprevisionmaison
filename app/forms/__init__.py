4bc846d813f4189f64eb87d9f5d9037b7ab50b66
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-08 10:40:03 +0200

    add user in __init__ form

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,4 +2,5 @@ from .category_form import CategoryForm
 from .product_form import ProductForm
 from .previsions_form import PrevisionForm
 from .acheter_form import AcheterForm
+from .user_form import UserForm
 
