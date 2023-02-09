47c8ebca769018b63c3f0ac34cebd0aa89dec5c9
Arsenicpoison <arseneirankunda7@gmail.com>
2023-02-09 22:59:01 +0200

    add a ajax method to get product_name

--- app/static/app/js/main.js
+++ app/static/app/js/main.js
@@ -1,3 +1,18 @@
+// get product_name by category name 
+$(function() {
+    $("#category_id").on("change", function() {
+        category_id = $(this).val();
+        $.get(
+            "/previsions/getProducts", {
+                category_id: category_id
+            },
+            function(data) {
+                $("id_product").html(data)
+            }
+        );
+    });
+
+});
 // calculate total price of a product
 $('#id_unit_price').on('keyup', function() {
     var unit_price = $(this).val();
@@ -5,3 +20,4 @@ $('#id_unit_price').on('keyup', function() {
     var total = id_quantite * unit_price;
     $('#id_total_price').val(total);
 });
+p
